1dbb5939168a3bf1d99c54ad33cb84ed5c703c8d
SDS-005 <sds-005@70-12-241-102.pools.spcsdns.net>
2017-04-25 14:05:03 +0900

    Initial Commit

---
+++ storyboardPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  storyboardPractice
+//
+//  Created by SDS-005 on 2017. 4. 25..
+//  Copyright © 2017년 SDS-005. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
